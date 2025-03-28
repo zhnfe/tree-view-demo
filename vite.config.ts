67d0172bb1c23fcf8e8b1dacbc35669067395947
haonan <work.haonan@gmail.com>
2025-03-28 13:09:45 +0800

    config base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/tree-view-demo/'
 })
